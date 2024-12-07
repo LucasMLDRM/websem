3cc2c043b4cbd762a65215ae5a99350a2d7d67bf
LucasMLDRM <108149514+LucasMLDRM@users.noreply.github.com>
2024-12-07 01:00:05 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './screens/Login/components/login/login';
+import Dashboard from './screens/Dashboard/dashboard';
+import ProtectedRoute from './screens/Login/ProtectedRoute.tsx';
+
+const App = () => {
+    return (
+        <Router>
+            <div className='app'>
+                <Routes>
+                    <Route path="/" element={<Login/>}/>
+                    <Route path="/dashboard/*" 
+                           element={
+                           <ProtectedRoute>
+                            <Dashboard/>
+                           </ProtectedRoute>}/>
+                </Routes>
+            </div>
+        </Router>
+    );
+};
+
+export default App;
